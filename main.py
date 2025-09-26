d926c175d9f4f3a3849d9150bee8612a55b41269
Petrovich <yahnovets_o@rocketdata.io>
2025-09-26 20:18:08 +0300

    chore: initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+import sys
+from aiogram import Bot, Dispatcher, types, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.filters import CommandStart
+from aiogram.enums import ParseMode
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start(message: types.Message) -> None:
+    await message.answer(f"HI!! {html.bold(message.from_user.full_name)}")
+
+async def main():
+    bot = Bot(token="",
+              default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
