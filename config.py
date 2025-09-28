dae3d2ab619265d2c83db0b7e2005f89aefb1ebb
yakhovets.o <yakhovetso@gmail.com>
2025-09-28 15:47:56 +0300

    feat: add features in tg config

--- config.py
+++ config.py
@@ -1,15 +1,20 @@
-from pydantic import SecretStr
+from pydantic import SecretStr, EmailStr
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class BaseConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8", extra="ignore")
+    model_config = SettingsConfigDict(
+        env_file=".env", env_file_encoding="utf-8", extra="ignore"
+    )
+
 
 class TelegramConfig(BaseConfig):
     model_config = SettingsConfigDict(env_prefix="TG_")
 
     token: SecretStr
+    admin: str
+    support: str
+    email: EmailStr
 
 
-
-config = TelegramConfig()
+tg_config = TelegramConfig()
