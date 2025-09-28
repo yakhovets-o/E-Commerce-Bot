6795b71ff10f1184b4b571ebedf5b9803e9c64cd
yakhovets.o <yakhovetso@gmail.com>
2025-09-28 15:49:12 +0300

    delete

--- handlers/commands.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, types, html
-from aiogram.filters import CommandStart
-
-router = Router(name=__name__)
-
-
-@router.message(CommandStart())
-async def start(message: types.Message) -> None:
-    await message.answer(f"HI!! {html.bold(message.from_user.full_name)}")
